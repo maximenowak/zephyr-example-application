e59ce2b991a41174b220ad12b1884b8a56c633db
Maxime Nowak <maxime.nowak@gmail.com>
2025-08-12 16:23:11 +0200

    fix: comment also unused variable

--- app/src/main.c
+++ app/src/main.c
@@ -19,7 +19,7 @@ LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 int main(void)
 {
 	int ret;
-	int new_variable = 42;
+	//int new_variable = 42;
 	//printk("Print a variable %d\n", new_variable);
 
 	unsigned int period_ms = BLINK_PERIOD_MS_MAX;
