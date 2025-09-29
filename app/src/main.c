49492f9843c1b96cffe123f6affcb42c2bf8b255
Maxime Nowak <maxime.nowak@gmail.com>
2025-09-29 13:55:41 +0200

    Update main.c

--- app/src/main.c
+++ app/src/main.c
@@ -21,6 +21,8 @@ int main(void)
 	int ret;
 	int new_variable = 42;
 	printk("Print a variable %d\n", new_variable);
+	printk("Print a variable %d\n", new_variable);
+
 
 	unsigned int period_ms = BLINK_PERIOD_MS_MAX;
 	const struct device *sensor, *blink;
